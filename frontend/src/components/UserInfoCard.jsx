c66c01f7a0963f10f63a641d7ab32bb68dfc2d3e
kaanmetep <kaanpmete@gmail.com>
2024-05-25 16:36:46 +0300

    working on deleting a match func

--- frontend/src/components/UserInfoCard.jsx
+++ frontend/src/components/UserInfoCard.jsx
@@ -21,6 +21,9 @@ function UserInfoCard({ setSelectedUser, selectedUser }) {
             <ion-icon name="logo-instagram"></ion-icon>
             <p>{selectedUser.personelDetails.instagramUsername}</p>
           </div>
+          <button className="bg-red-400 text-white py-1 px-4 rounded-md font-semibold uppercase text-sm hover:bg-red-600 transition-all delay-[50ms]">
+            Delete this match
+          </button>
         </div>
         <button
           className="absolute right-3 top-3 aspect-auto  text-slate-400  rounded-full text-lg flex items-center justify-center hover:text-slate-700 transition-all delay-[50ms] font-bold"
